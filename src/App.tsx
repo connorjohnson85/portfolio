30fb4ab3f48e09cc783159f0412b3745f9276ae6
connorjohnson85 <connorjohnson853@gmail.com>
2024-01-14 22:29:23 -0700

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
+import { AppBar, Box, Button, Divider, Grid, IconButton, Paper, Toolbar, Typography} from '@mui/material'
+import { styled } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static" >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Connor's Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box p={1}>
+      <Box sx={{ display: 'flex' }}>
+        <Box width={'100%'} marginRight={1}>
+          <Paper style={{height: 'full', padding: 2}} elevation={3}>
+            <Box py={2} px={1} width={'100%'}>
+            <Box sx={{ display: 'flex' }}>
+              <Box px={2} width={'100%'}>Professional Projects</Box>
+            </Box>
+            </Box>
+            <Divider/>
+          </Paper>
+        </Box>
+        <Box width={'100%'} marginLeft={1}>
+          <Paper style={{height: 'full', padding: 2}} elevation={3}>
+            <Box py={2} px={1} width={'100%'}>
+            <Box sx={{ display: 'flex' }}>
+              <Box px={2} width={'100%'}>Projects For Fun</Box>
+            </Box>
+            </Box>
+            <Divider/>
+          </Paper>
+        </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
