1afa5f6f94755124f75b49acd19f572660807a64
connorjohnson85 <connorjohnson853@gmail.com>
2024-01-16 22:47:43 -0700

    ticTacToe changes

--- src/pages/PersonalProjects/ticTacToe/ticTacToeBoard.tsx
+++ src/pages/PersonalProjects/ticTacToe/ticTacToeBoard.tsx
@@ -1,6 +1,4 @@
-import { AppBar, Box, Button, Paper, Stack, Toolbar, Typography, styled} from '@mui/material'
-import { Link } from 'react-router-dom';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Box, Button, Paper, Stack, Typography, styled} from '@mui/material'
 import { useEffect, useState } from 'react';
 
 
@@ -19,53 +17,61 @@ function TicTacToeBoard() {
   const [player1Turn, setPlayer1Turn] = useState(true)
   const [winState, setWinState] = useState('')
 
+  const clear = () => {
+    setWinState('')
+    setPlayer1Turn(true)
+    setRow1([0, 0 , 0])
+    setRow2([0, 0, 0])
+    setRow3([0, 0, 0])
+  }
+
   const checkWin = () => {
-    if (row1[0] == 1 && row1[1] == 1 && row1[2] == 1) {
+    if (row1[0] === 1 && row1[1] === 1 && row1[2] === 1) {
         setWinState('Player 1')
     }
-    if (row2[0] == 1 && row2[1] == 1 && row2[2] == 1) {
+    if (row2[0] === 1 && row2[1] === 1 && row2[2] === 1) {
         setWinState('Player 1')
     }
-    if (row3[0] == 1 && row3[1] == 1 && row3[2] == 1) {
+    if (row3[0] === 1 && row3[1] === 1 && row3[2] === 1) {
         setWinState('Player 1')
     }
-    if (row1[0] == 1 && row2[1] == 1 && row3[2] == 1) {
+    if (row1[0] === 1 && row2[1] === 1 && row3[2] === 1) {
         setWinState('Player 1')
     }
-    if (row1[2] == 1 && row2[1] == 1 && row3[0] == 1) {
+    if (row1[2] === 1 && row2[1] === 1 && row3[0] === 1) {
         setWinState('Player 1')
     }
-    if (row1[0] == 1 && row2[0] == 1 && row3[0] == 1) {
+    if (row1[0] === 1 && row2[0] === 1 && row3[0] === 1) {
         setWinState('Player 1')
     }
-    if (row1[1] == 1 && row2[1] == 1 && row3[1] == 1) {
+    if (row1[1] === 1 && row2[1] === 1 && row3[1] === 1) {
         setWinState('Player 1')
     }
-    if (row1[2] == 1 && row2[2] == 1 && row3[2] == 1) {
+    if (row1[2] === 1 && row2[2] === 1 && row3[2] === 1) {
         setWinState('Player 1')
     }
-    if (row1[0] == 2 && row1[1] == 2 && row1[2] == 2) {
+    if (row1[0] === 2 && row1[1] === 2 && row1[2] === 2) {
         setWinState('Player 2')
     }
-    if (row2[0] == 2 && row2[1] == 2 && row2[2] == 2) {
+    if (row2[0] === 2 && row2[1] === 2 && row2[2] === 2) {
         setWinState('Player 2')
     }
-    if (row3[0] == 2 && row3[1] == 2 && row3[2] == 2) {
+    if (row3[0] === 2 && row3[1] === 2 && row3[2] === 2) {
         setWinState('Player 2')
     }
-    if (row1[0] == 2 && row2[1] == 2 && row3[2] == 2) {
+    if (row1[0] === 2 && row2[1] === 2 && row3[2] === 2) {
         setWinState('Player 2')
     }
-    if (row1[2] == 2 && row2[1] == 2 && row3[0] == 2) {
+    if (row1[2] === 2 && row2[1] === 2 && row3[0] === 2) {
         setWinState('Player 2')
     }
-    if (row1[0] == 2 && row2[0] == 2 && row3[0] == 2) {
+    if (row1[0] === 2 && row2[0] === 2 && row3[0] === 2) {
         setWinState('Player 2')
     }
-    if (row1[1] == 2 && row2[1] == 2 && row3[1] == 2) {
+    if (row1[1] === 2 && row2[1] === 2 && row3[1] === 2) {
         setWinState('Player 2')
     }
-    if (row1[2] == 2 && row2[2] == 2 && row3[2] == 2) {
+    if (row1[2] === 2 && row2[2] === 2 && row3[2] === 2) {
         setWinState('Player 2')
     }
   }
@@ -84,7 +90,6 @@ function TicTacToeBoard() {
     }
   }, [player1Turn])
 
-  console.log(row1)
   return (
       <Box>
         <Typography variant='h4'>
@@ -92,13 +97,13 @@ function TicTacToeBoard() {
         </Typography>
         <Stack spacing={1}>
             <Stack spacing={2} direction={'row'}>
-                {row1.map((item, step) => <TicTacToePiece identity={`row1${step}`} index={step} value={item == 1 ? 'x' : item == 2 ? 'o' : ' '} row={row1} setRow={setRow1} playerTurn={player1Turn} setTurn={setPlayer1Turn}/>)}
+                {row1.map((item, step) => <TicTacToePiece identity={`row1${step}`} index={step} value={item === 1 ? 'x' : item === 2 ? 'o' : ' '} row={row1} setRow={setRow1} playerTurn={player1Turn} setTurn={setPlayer1Turn}/>)}
             </Stack>
             <Stack spacing={2} direction={'row'}>
-                {row2.map((item, step) => <TicTacToePiece identity={`row2${step}`} index={step}  value={item == 1 ? 'x' : item == 2 ? 'o' : ' '} row={row2} setRow={setRow2} playerTurn={player1Turn} setTurn={setPlayer1Turn}/>)}
+                {row2.map((item, step) => <TicTacToePiece identity={`row2${step}`} index={step}  value={item === 1 ? 'x' : item === 2 ? 'o' : ' '} row={row2} setRow={setRow2} playerTurn={player1Turn} setTurn={setPlayer1Turn}/>)}
             </Stack>
             <Stack spacing={2} direction={'row'}>
-                {row3.map((item, step) => <TicTacToePiece identity={`row3${step}`} index={step}  value={item == 1 ? 'x' : item == 2 ? 'o' : ' '} row={row3} setRow={setRow3} playerTurn={player1Turn} setTurn={setPlayer1Turn}/> )}
+                {row3.map((item, step) => <TicTacToePiece identity={`row3${step}`} index={step}  value={item === 1 ? 'x' : item === 2 ? 'o' : ' '} row={row3} setRow={setRow3} playerTurn={player1Turn} setTurn={setPlayer1Turn}/> )}
             </Stack>
         </Stack>
       </Box>
@@ -108,7 +113,7 @@ function TicTacToeBoard() {
 const TicTacToePiece = ({ value, identity, setRow, index, row, playerTurn, setTurn }: { value: string, identity: string, row: number[], index: number, setRow: (newValue: number[]) => void, playerTurn: boolean, setTurn: (newValue: boolean) => void}) => <>
     <Item key={identity}>
         <Button onClick={() => {
-            if (value == ' ') {
+            if (value === ' ') {
                 if(playerTurn) {
                     let newArray = row
                     newArray[index] = 1
